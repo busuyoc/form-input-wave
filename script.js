65f31161ca25600cbeb6a2a8c073aa991abd727e
Busuioc Petru <busuioc.petru.claudiu@gmail.com>
2024-04-19 16:42:30 +0300

    asda

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ labels.forEach((label) => {
   label.innerHTML = label.innerText
     .split("")
     .map((letter, index) => {
-      `<span style="transition-delay:${index * 50}ms">${letter}</span>`;
+      return `<span style="transition-delay:${index * 50}ms">${letter}</span>`;
     })
     .join("");
 });
